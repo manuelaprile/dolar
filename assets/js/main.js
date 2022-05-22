dde441484a8bffb51e2e365041f35c0b62618303
manuel <manuelaprile01@hotmail.com>
2022-05-22 16:08:55 -0300

    mas dolares agregados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
 const seleccionar = () => {
   let selector = document.querySelector("#selector").value;
+  const dolares = ['Dolar Oficial', 'Dolar Blue', 'Dolar Soja', 'Dolar Contado con Liqui', 'Dolar turista', 'Bitcoin', 'Dolar Bolsa', 'Dolar cotiza'];
   let resultado = document.querySelector('.result');
   let dolar;
   let btn = document.querySelector('.btn-limpiar');
-  (selector === 'oficial') ? dolar = 'Dolar Oficial' : '';
-  (selector === 'blue') ? dolar = 'Dolar Blue' : '';
-  (selector === 'liqui') ? dolar = 'Dolar Contado con Liqui' : '';
 
   //Peticion con AXIOS
   axios.get('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
     .then(function (response) {
       for (let i of response.data) {
+        (selector === 'Dolar Oficial') ? dolar = 'Dolar Oficial' : '';
+        (selector === 'Dolar Blue') ? dolar = 'Dolar Blue' : '';
+        (selector === 'Dolar Contado con Liqui') ? dolar = 'Dolar Contado con Liqui' : '';
+        (selector === 'Bitcoin') ? dolar = 'Bitcoin' : '';
+        (selector === 'Dolar Soja') ? dolar = 'Dolar Soja' : '';
+        (selector === 'Dolar Bolsa') ? dolar = 'Dolar Bolsa' : '';
+        (selector === 'Dolar Turista') ? dolar = 'Dolar turista' : '';
         if (dolar === i.casa.nombre) {
           var compra = document.createElement('p');
           var venta = document.createElement('p');
@@ -21,7 +26,7 @@ const seleccionar = () => {
           btn.addEventListener('click', () => {
             resultado.appendChild(compra).remove();
             resultado.appendChild(venta).remove();
-          } );
+          });
         }
       }
     })
